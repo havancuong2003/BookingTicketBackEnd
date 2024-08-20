4589d0a35864daa0eeb33ead22cc3fbd776f4f79
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-08-20 17:00:28 +0700

    add: add login jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+     AuthModule,
+      UserModule,
+      PrismaModule,
+    ],
+  controllers: [UserController],
+  providers: [PrismaService],
 })
 export class AppModule {}
