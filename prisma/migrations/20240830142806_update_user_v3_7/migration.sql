f6b95d39d96fbd6a9d94b33102a47b41b8648e42
ngvo.1203@gmail.com <ngvo.1203@gmail.com>
2024-09-04 14:07:47 +0700

    add booking ticket

---
+++ prisma/migrations/20240830142806_update_user_v3_7/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "resetPasswordExpires" TIMESTAMP(3),
+ADD COLUMN     "resetPasswordToken" TEXT,
+ADD COLUMN     "verificationToken" TEXT,
+ADD COLUMN     "verificationTokenExpires" TIMESTAMP(3);
